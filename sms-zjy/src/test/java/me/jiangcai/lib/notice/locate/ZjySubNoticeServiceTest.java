6198fbb3f53657dd86fed56a851a863dc5c74175
Cai Jiang <luffy.ja@gmail.com>
2017-05-20 18:04:00 +0800

    当时要单独发布的消息

--- sms-zjy/src/test/java/me/jiangcai/lib/notice/locate/ZjySubNoticeServiceTest.java
+++ sms-zjy/src/test/java/me/jiangcai/lib/notice/locate/ZjySubNoticeServiceTest.java
@@ -9,14 +9,13 @@ import me.jiangcai.lib.notice.dhst.Fee;
 import me.jiangcai.lib.test.SpringWebTest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.EventListener;
 import org.springframework.test.context.ContextConfiguration;
-
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +32,7 @@ public class ZjySubNoticeServiceTest extends SpringWebTest {
     private NoticeService noticeService;
 
     @Test
+    @Ignore
     public void hello() {
         //  //账号：meiyueyzm  密码：meiyue6629     请提交正常短信内容（验证码类），建议不要出现测试等字样，签名是平台自动加的
 //        noticeService.send("zjy://", new To() {
@@ -47,18 +47,30 @@ public class ZjySubNoticeServiceTest extends SpringWebTest {
 //            }
 //        });
 
-        noticeService.send("zjy://", new To() {
-            @Override
-            public String mobilePhone() {
-                return "18606509616";
-            }
-        }, new Content() {
-            @Override
-            public String asText() {
-                // 【款爷二手】
-                return "您好，您的验证码是" + UUID.randomUUID().toString().substring(0, 5) + "，请勿泄露给他人。";
-            }
-        });
+        // 文君 18381850466
+        for (String number : new String[]{
+                "18381850466"
+//                "13588596617"
+//                "15988289495", "15988207785", "13486582577", "15968517705", "15958525571", "18757552573"
+        }) {
+            noticeService.send("zjy://", new To() {
+                @Override
+                public String mobilePhone() {
+//                return "18606509616";
+//                    return "13588596617";
+                    return number;
+                }
+            }, new Content() {
+                @Override
+                public String asText() {
+                    // 【款爷二手】
+//                return "您好，您的验证码是" + UUID.randomUUID().toString().substring(0, 5) + "，请勿泄露给他人。";
+//                return "您的款爷帐号和所有申请都已经注销；如需继续使用款爷请重新注册。";
+                    return "尊敬的文君，您在款爷网商宝的申请已撤销。祝您生活愉快";
+                }
+            });
+        }
+
 
 //        【美约科技】亲爱的会员您好，您的注册验证码是123456，请勿泄露给他人。
 
